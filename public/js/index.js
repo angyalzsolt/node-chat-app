f602148ae1f4d7925041e70359e94b57fe10db05
angyalzsolt <angyal.zsolt@hotmail.hu>
2019-02-20 11:10:30 +0100

    Add css for chat page

--- public/js/index.js
+++ public/js/index.js
@@ -34,11 +34,14 @@ socket.on('newLocationMessage', (message) => {
 $('#message-form').on('submit', (e) => {
 	e.preventDefault();
 
+
+	const messageTextbox = $('[name=message]');
+
 	socket.emit('createMessage', {
 		from: 'User',
-		text: $('[name=message]').val()
+		text: messageTextbox.val()
 	}, () => {
-
+		messageTextbox.val('');
 	});
 });
 
@@ -47,14 +50,19 @@ let locationButton = $('#send-location');
 locationButton.on('click', () => {
 	if(!navigator.geolocation) {
 		return alert('Geolocation not supported by your browser.');
-	}
+	};
+
+
+	locationButton.attr('disabled', 'disabled').text('Sending location...');
 
 	navigator.geolocation.getCurrentPosition((position)=> {
+		locationButton.removeAttr('disabled').text('Send location');
 		socket.emit('createLocationMessage', {
 			latitude: position.coords.latitude,
 			longitude: position.coords.longitude
 		});
 	}, () => {
+		locationButton.removeAttr('disabled').text('Send location');
 		alert('Unable to fetch location.');
 	})
 })
